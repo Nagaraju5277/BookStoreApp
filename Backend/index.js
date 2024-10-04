48e367031b4f95bd137bff0db713cb4b8024d659
Nagaraju5277 <Kannanagaraju225@gmail.com>
2024-10-04 22:34:29 +0530

    Backend Done!!

---
+++ Backend/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import cors from "cors"
+
+import bookRoute from "./routes/book.route.js"
+import userRoute from "./routes/user.route.js"
+
+
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT =process.env.PORT || 4000
+const URI=process.env.MONGODBURI
+
+// connect to mongoDB
+try{
+    mongoose.connect(URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    console.log("Connected to MongoDb")
+}catch(error){
+    console.log("Error",error)
+}
+// Routes
+app.use("/book",bookRoute)
+app.use("/user",userRoute)
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 function Login() {
     const {
@@ -9,21 +11,46 @@ function Login() {
         formState: { errors },
     } = useForm()
 
-    const onSubmit = (data) => console.log(data)
+    const onSubmit =async (data) => {
+        const userInfo = {
+            email: data.email,
+            password: data.password
+        }
+        await axios.post("http://localhost:4001/user/login", userInfo)
+            .then((res) => {
+                console.log(res.data)
+                if (res.data) {
+                    toast.success('Logged in succesfully');
+                    document.getElementById("my_modal_3").close();
+                    setTimeout(()=>{
+                        window.location.reload()
+                        localStorage.setItem("Users",JSON.stringify( res.data.user))
+
+                    },1000)
+                }
+            }).catch((error) => {
+                if (error.response) {
+                    console.log(error)
+                    toast.error('Error:" + error.response.data.message');
+                    setTimeout(()=>{},2000)
+                }
+            })
+    }
     return (
-        <div>
-            <dialog id="my_modal_3" className="modal">
-                <div className="modal-box">
+        <div className='bg-blue-200'>
+            <dialog id="my_modal_3" className="modal ">
+                <div className="modal-box dark:bg-slate-900 dark:text-white">
                     <form onSubmit={handleSubmit(onSubmit)} method="dialog">
                         {/* if there is a button in form, it will close the modal */}
-                        <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
+                        <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        onClick={()=>document.getElementById("my_modal_3").close()}>✕</Link>
                     
                     <h3 className="font-bold text-lg">Login</h3>
                     {/* email */}
                     <div className='mt-4 space-y-2'>
                         <span>Email</span>
                         <br />
-                        <input type="email" placeholder='Enter your email' className='w-80 px-3 py-1 border rounded-md outline-none'
+                        <input type="email" placeholder='Enter your email' className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white dark:border dark:border-slate-700'
                             {...register("email", { required: true })}
                         />
                         <br />
@@ -33,7 +60,7 @@ function Login() {
                     <div className='mt-4 space-y-2'>
                         <span>Password</span>
                         <br />
-                        <input type="text" placeholder='Enter your password' className='w-80 px-3 py-1 border rounded-md outline-none'
+                        <input type="text" placeholder='Enter your password' className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white dark:border dark:border-slate-700'
                             {...register("password", { required: true })}
                         />
                         <br />
@@ -41,7 +68,7 @@ function Login() {
                     </div>
                     <div className='flex justify-around mt-4'>
                         {/* button */}
-                        <button className='bg-pink-500 text-white  px-4 py-1 rounded-md hover:bg-pink-700 duration-200'>Login</button>
+                        <button className='bg-pink-500 text-white dark:bg-pink-700 dark:text-white dark:border dark:border-pink-700  px-4 py-1 rounded-md hover:bg-pink-700 duration-200'>Login</button>
 
                         <p>Not registered?
                             <Link to="/signup" className='underline text-blue-500 cursor-pointer'>
